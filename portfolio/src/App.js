2e6a0da7f5d293e60bdd8d0a1b780ec28624d938
matt15y <matt15y@gmail.com>
2020-11-25 21:47:44 -0600

    added Navbar.js

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				<Navbar />
+			</header>
+		</div>
+	);
+}
+
+export default App;
